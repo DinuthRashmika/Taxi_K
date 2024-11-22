2e939e20d07c1a58182050b4ad8418ada9e3dfb1
kavishka286 <kavishkasenavirathna6@gmail.com>
2024-11-22 10:54:20 +0530

    Create AboutUs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './containers/Main/HomePage'
 import ContactForm from './containers/Main/ContactForm'
+import AboutUs from './containers/Main/AboutUs';
 
 function App() {
   return (
@@ -12,6 +13,10 @@ function App() {
 
       <Routes>
         <Route path="/ContactPage" element={<ContactForm/>} />
+      </Routes>
+
+      <Routes>
+        <Route path="/AboutUs" element={<AboutUs/>} />
       </Routes>
 
     </Router>
